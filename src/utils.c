01a1fee7248aa559febe837442a6cdcedf4c934b
gasolineeater <ezekaj@42.heilbronn.de>
2025-04-01 23:22:28 +0200

    Add: Initalizes an empty stack.

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,13 @@
-#include "push_swap.h"
+#include "../inc/push_swap.h"
+
+t_stack	*init_stack(void)
+{
+	t_stack	*stack;
+
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
+	stack->top = NULL;
+	stack->size = 0;
+	return (stack);
+}
