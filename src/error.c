01ae7515feaddb5be0ddaa0966957b7b1ece2a38
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-13 01:32:24 +0200

    Add: Implementation of free_stack

--- src/error.c
+++ src/error.c
@@ -5,3 +5,18 @@ void	error_exit(void)
 	ft_putendl_fd("Error", 2);
 	exit(1);
 }
+
+void	free_stack(t_stack *stack)
+{
+	t_node	*temp;
+
+	if (!stack)
+		return ;
+	while (stack->top)
+	{
+		temp = stack->top;
+		stack->top = stack->top->next;
+		free(temp);
+	}
+	free(stack);
+}
