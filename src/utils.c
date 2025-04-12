e31a12ce11635357f6b4100aaef41ece06a88c6f
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-12 22:39:03 +0200

    Add: parse_input function

--- src/utils.c
+++ src/utils.c
@@ -11,3 +11,15 @@ t_stack	*init_stack(void)
 	stack->size = 0;
 	return (stack);
 }
+
+char	**parse_input(int ac, char **av)
+{
+	char	**input;
+
+	if (ac == 1)
+		return (NULL);
+	input = ft_split(av[1], ' ');
+	if (!input)
+		return (NULL);
+	return (input);
+}
