bc78f872b9710bc60c23c36ce5226551cf423864
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-13 17:33:15 +0200

    Add: Implementation of sb.

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -12,3 +12,16 @@ void	sa(t_stack *stack_a)
 	stack_a->top->next = temp;
 	ft_printf("sa\n");
 }
+
+void	sb(t_stack *stack_b)
+{
+	t_node	*temp;
+
+	if (stack_b == NULL || stack_b->top == NULL || stack_b->top->next == NULL)
+		return ;
+	temp = stack_b->top;
+	stack_b->top = stack_b->top->next;
+	temp->next = stack_b->top->next;
+	stack_b->top->next = temp;
+	ft_printf("sb\n");
+}
