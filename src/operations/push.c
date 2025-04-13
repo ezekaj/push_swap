d25cdb2815875143d5b7a7145a874d0cd17ffe85
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-13 17:18:25 +0200

    Add: Implementation of pa.

--- src/operations/push.c
+++ src/operations/push.c
@@ -1,2 +1,16 @@
 #include "../../inc/push_swap.h"
 
+void	pa(t_stack *stack_a, t_stack *stack_b)
+{
+	t_node	*temp;
+
+	if (stack_b == NULL || stack_b->top == NULL)
+		return ;
+	temp = stack_b->top;
+	stack_b->top = stack_b->top->next;
+	temp->next = stack_a->top;
+	stack_a->top = temp;
+	stack_a->size++;
+	stack_b->size--;
+	ft_printf("pa\n");
+}
