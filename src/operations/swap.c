a2216b307f7cb76e018c45589664cf13350d5bc0
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-13 17:26:06 +0200

    Add: Implementation of sa.

--- src/operations/swap.c
+++ src/operations/swap.c
@@ -1,1 +1,14 @@
 #include "../../inc/push_swap.h"
+
+void	sa(t_stack *stack_a)
+{
+	t_node	*temp;
+
+	if (stack_a == NULL || stack_a->top == NULL || stack_a->top->next == NULL)
+		return ;
+	temp = stack_a->top;
+	stack_a->top = stack_a->top->next;
+	temp->next = stack_a->top->next;
+	stack_a->top->next = temp;
+	ft_printf("sa\n");
+}
