f9a6fbf89827bf0d178f41bcd48a7153a228a3ed
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-13 17:18:59 +0200

    Add: Implementation of pb.

--- src/operations/push.c
+++ src/operations/push.c
@@ -14,3 +14,18 @@ void	pa(t_stack *stack_a, t_stack *stack_b)
 	stack_b->size--;
 	ft_printf("pa\n");
 }
+
+void	pb(t_stack *stack_a, t_stack *stack_b)
+{
+	t_node	*temp;
+
+	if (stack_a == NULL || stack_a->top == NULL)
+		return ;
+	temp = stack_a->top;
+	stack_a->top = stack_a->top->next;
+	temp->next = stack_b->top;
+	stack_b->top = temp;
+	stack_b->size++;
+	stack_a->size--;
+	ft_printf("pb\n");
+}
