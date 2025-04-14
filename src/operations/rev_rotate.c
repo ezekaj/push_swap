0eafb805527ef3a20df90d876e23e0519e7870e4
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-14 18:01:58 +0200

    Add: Implementation of rra, rrb, rrr.

--- src/operations/rev_rotate.c
+++ src/operations/rev_rotate.c
@@ -18,3 +18,22 @@ void	rev_rotate(t_stack *stack)
 	tmp->next = stack->top;
 	stack->top = tmp;
 }
+
+void	rra(t_stack *stack_a)
+{
+	rev_rotate(stack_a);
+	ft_printf("rra\n");
+}
+
+void	rrb(t_stack *stack_b)
+{
+	rev_rotate(stack_b);
+	ft_printf("rrb\n");
+}
+
+void	rrr(t_stack *stack_a, t_stack *stack_b)
+{
+	rev_rotate(stack_a);
+	rev_rotate(stack_b);
+	ft_printf("rrr\n");
+}
