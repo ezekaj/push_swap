373fd69e049bc36e40f59e6ac2dbfca877bd306f
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-14 18:26:16 +0200

    Add: Implementation of sort2.

--- src/sort/small_sort.c
+++ src/sort/small_sort.c
@@ -1,1 +1,9 @@
 #include "../../inc/push_swap.h"
+
+void	sort2(t_stack *stack_a)
+{
+	if (stack_a == NULL || stack_a->top == NULL || stack_a->top->next == NULL)
+		return ;
+	if (stack_a->top->data > stack_a->top->next->data)
+		sa(stack_a);
+}
