575cc237474ca6a7bd273ebd30f50b63a0388268
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-14 22:46:30 +0200

    Add: Implementation of sort3

--- src/sort/small_sort.c
+++ src/sort/small_sort.c
@@ -7,3 +7,22 @@ void	sort2(t_stack *stack_a)
 	if (stack_a->top->data > stack_a->top->next->data)
 		sa(stack_a);
 }
+
+void	sort3(t_stack *stack_a)
+{
+	if (stack_a == NULL || stack_a->top == NULL || stack_a->top->next == NULL || stack_a->top->next->next == NULL)
+		return ;
+	if (stack_a->top->data < stack_a->top->next->data && stack_a->top->next->data < stack_a->top->next->next->data)
+		return ;
+	else if (stack_a->top->data > stack_a->top->next->data && stack_a->top->next->data < stack_a->top->next->next->data)
+		sa(stack_a);
+	else if (stack_a->top->data < stack_a->top->next->data && stack_a->top->next->data > stack_a->top->next->next->data)
+		rra(stack_a);
+	else if (stack_a->top->data > stack_a->top->next->data && stack_a->top->next->data > stack_a->top->next->next->data)
+	{
+		sa(stack_a);
+		rra(stack_a);
+	}
+	else if (stack_a->top->next->data > stack_a->top->data && stack_a->top->next->data > stack_a->top->next->next->data))
+		ra(stack_a);
+}
