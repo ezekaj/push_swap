cca9e66d8fa655d5ad3774d5d499bdd8817b50ce
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-14 15:50:13 +0200

    Add: Implementation of rotate function.

--- src/operations/rotate.c
+++ src/operations/rotate.c
@@ -1,1 +1,15 @@
 #include "../../inc/push_swap.h"
+
+void	rotate(t_stack *stack)
+{
+	t_node	*tmp;
+
+	if (stack == NULL || stack->top == NULL || stack->top->next == NULL)
+		return ;
+	tmp = stack->top;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = stack->top;
+	stack->top = stack->top->next;
+	tmp->next->next = NULL;
+}
