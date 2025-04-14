f2eb43633ddbda27b505b7e6e07bc613c2eff40e
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-14 17:59:55 +0200

    Add: Implementation of rev_rotate

---
+++ src/operations/rev_rotate.c
@@ -0,0 +1,20 @@
+#include "../../inc/push_swap.h"
+
+void	rev_rotate(t_stack *stack)
+{
+	t_node	*tmp;
+	t_node	*sec_last;
+
+	if (stack == NULL || stack->top == NULL || stack->top->next == NULL)
+		return ;
+	tmp = stack->top;
+	sec_last = NULL;
+	while (tmp->next)
+	{
+		sec_last = tmp;
+		tmp = tmp->next;
+	}
+	sec_last->next = NULL;
+	tmp->next = stack->top;
+	stack->top = tmp;
+}
