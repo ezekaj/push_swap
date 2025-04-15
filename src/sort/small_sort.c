5eb89ef1197be51514b67011d74b02f69215db51
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-15 14:44:39 +0200

    Add: Starting the implementation of sort4 but trying different approaches i came to the conclusion we need a helper function to implement a find_min_pos so we can use that to decrement the array from 4 to 3 numbers and use sort3.

--- src/sort/small_sort.c
+++ src/sort/small_sort.c
@@ -1,5 +1,31 @@
 #include "../../inc/push_swap.h"
 
+static int	find_min_pos(t_stack *stack)
+{
+	t_node	*current;
+	int		min;
+	int		pos;
+	int		min_pos;
+
+	if (stack == NULL || stack->top == NULL)
+		return (0);
+	current = stack->top;
+	min = current->data;
+	pos = 0;
+	min_pos = 0;
+	while (current)
+	{
+		if (current->data < min)
+		{
+			min = current->data;
+			min_pos = pos;
+		}
+		pos++;
+		current = current->next;
+	}
+	return(min_pos);
+}
+
 void	sort2(t_stack *stack_a)
 {
 	if (stack_a == NULL || stack_a->top == NULL || stack_a->top->next == NULL)
@@ -23,6 +49,12 @@ void	sort3(t_stack *stack_a)
 		sa(stack_a);
 		rra(stack_a);
 	}
-	else if (stack_a->top->next->data > stack_a->top->data && stack_a->top->next->data > stack_a->top->next->next->data))
+	else if (stack_a->top->next->data > stack_a->top->data && stack_a->top->next->data > stack_a->top->next->next->data)
 		ra(stack_a);
+}
+
+void	sort4(t_stack *stack_a, t_stack *stack_b)
+{
+	if (stack_a == NULL || stack_a->top == NULL || stack_a->top->next == NULL || stack_a->top->next->next == NULL || stack_a->top->next->next->next == NULL)
+		return ;
 }
