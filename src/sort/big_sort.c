592fcd816200236bd201104b2893241115722d56
gasolineeater <ezekaj@student.42heilbronn.de>
2025-04-15 21:49:43 +0200

    Add: Started the main algorithm sort_big and created another function find_max_pos that is kinda the same as find_min_pos.

--- src/sort/big_sort.c
+++ src/sort/big_sort.c
@@ -1,1 +1,34 @@
 #include "../../inc/push_swap.h"
+
+int	find_max_pos(t_stack *stack)
+{
+	t_node	*current;
+	int		max;
+	int		pos;
+	int		max_pos;
+
+	if (stack == NULL || stack->top == NULL)
+		return (0);
+	current = stack->top;
+	max = current->data;
+	pos = 0;
+	max_pos = 0;
+	while (current)
+	{
+		if (current->data > max)
+		{
+			max = current->data;
+			max_pos = pos;
+		}
+		pos++;
+		current = current->next;
+	}
+	return (max_pos);
+}
+
+void	sort_big(t_stack *stack_a, t_stack *stack_b)
+{
+	if (stack_a == NULL || stack_b == NULL)
+		return ;
+	
+}
